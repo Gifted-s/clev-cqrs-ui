1f9be4364990738bfa14b41c003b1cfa7444f9ef
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2020-08-23 03:38:52 +0100

    modified styles.css, index.hbs and start.hbs

--- index.js
+++ index.js
@@ -15,31 +15,7 @@ let generatePage = (
     _this.generatePage = app
     router.get('/', async function (req, res, next) {
       const docs = await eventStore.getAll()
-      for (let i in docs) {
-        for (let j in docs[i].events) {
-          docs[i].events[j].eventTime = new Date(docs[i].events[j].eventTime).toUTCString()
-        }
-      }
-      let ar = []
-      for (let i in docs) {
-        for (let j in docs[i].events) {
-          for (let k of Object.keys(docs[i].events[j]).values()) {
-            if (k !== 'eventName' && k !== 'aggregateId' && k !== 'eventId' && k !== 'eventVersion' && k !== 'eventTime') {
-              ar.push(k + ' : ' + docs[i].events[j][k])
-              docs[i].events[j].data = ar
-            }
-          }
-          ar = []
-        }
-      }
       res.render('home/start', { title: 'Event Store', name: schemaName, event: docs })
-      setTimeout(() => {
-        for (let i in docs) {
-          for (let j in docs[i].events) {
-            delete docs[i].events[j].data
-          }
-        }
-      }, 1000)
     })
     return _this
   }
